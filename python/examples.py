4dd2fc8bf1335096e0b7d4d7120e7264a7cb8416
nobuhiro <e-dolphin@sannet.ne.jp>
2019-10-12 14:47:48 +0900

    ver2 to ver3

--- python/examples.py
+++ python/examples.py
@@ -1,23 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
+
 import sys
 sys.path.append('./')
 import e_Stat_API_Adaptor
 eStatAPI = e_Stat_API_Adaptor.e_Stat_API_Adaptor({
     # 取得したappId
-    'appId'	: '#appID#'    # データをダウンロード時に一度に取得するデータ件数
+    'appId'	: '5207fca5966c34abb05bb21c42eb09b16931660e'
+    # データをダウンロード時に一度に取得するデータ件数
     # next_keyに対応するか否か(非対応の場合は上記のlimitで設定した件数のみしかダウンロードされない)
-    , 'limit'	: '10000'    # 対応時はTrue/非対応時はFalse
-    , 'next_key'	: True        # 中間アプリの設置ディレクトリ
-    , 'directory': '#絶対パス#'        # APIのバージョン
-    , 'ver'		: '2.0'
+    , 'limit'	: '10000'
+    # 対応時はTrue/非対応時はFalse
+    , 'next_key'	: True
+    # 中間アプリの設置ディレクトリ
+    , 'directory': '/Users/nobuhiro/pycharmprojects/e-stat/python'
+    # APIのバージョン
+    , 'ver'		: '3.0'
 })
-#
-#
-# # インストール直後に下記を実行
-# #
+
+# インストール直後に下記を実行
+
 # 全ての統計表IDをローカルにダウンロード
-# print eStatAPI.load_all_ids()
+print(eStatAPI.load_all_ids())
+
 # ダウンロードした統計表IDからインデックスを作成
 # print eStatAPI.build_statid_index()
 #
